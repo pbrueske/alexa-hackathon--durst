3c4eac2cd2e617424718b3583b9f9fe0d335210e
Felix Seidel <felix@seidel.me>
2017-04-27 20:25:24 +0200

    Fork

--- flaschenpost.rb
+++ flaschenpost.rb
@@ -14,10 +14,12 @@ post '/' do
   when 'INTENT_REQUEST'
     case intent_request.intent.fetch('name')
     when 'ExpressOrder'
-    #  session.login_user
-    #  session.put_beer_in_cart
-    #  session.put_beer_in_cart
-    #  session.order_cart
+     Process.fork do
+       session.login_user
+       session.put_beer_in_cart
+       session.put_beer_in_cart
+       session.order_cart
+     end
 
      intent_response.add_speech("Deine Getränke sind auf dem Weg")
     when 'AddLineItem'
