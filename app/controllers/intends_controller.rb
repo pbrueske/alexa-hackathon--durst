de6618d321cec63ea8ee0dcd5deec05685142224
Peter Brüske <peter.brueske@zweitag.de>
2017-04-27 18:21:04 +0200

    React to diffentent intents

--- app/controllers/intends_controller.rb
+++ app/controllers/intends_controller.rb
@@ -1,9 +1,20 @@
 class IntendsController < ApplicationController
   def process(foo)
     request = AlexaRubykit::build_request(params)
-    
+
     response = AlexaRubykit::Response.new
-    response.add_speech("It's on!")
+    case request.type
+    when 'INTENT_REQUEST'
+      case request.intent.fetch('name')
+      when 'ExpressOrder'
+        response.add_speech("Du möchtest eine Bestellung aufgeben")
+      else
+        response.add_speech("Mit dieser Anfrage kann ich leider nichts anfangen")
+      end
+    when 'LAUNCH_REQUEST'
+      response.add_speech("Du hast eine Session gestartet")
+    end
+
     render json: response
   end
 end
