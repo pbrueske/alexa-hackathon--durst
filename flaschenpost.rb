bddb3164fa6b95060aed9bc10b03ccacd50bb068
Peter Brüske <peter.brueske@zweitag.de>
2017-04-27 19:41:58 +0200

    Add recognition of intent type and name

--- flaschenpost.rb
+++ flaschenpost.rb
@@ -8,6 +8,20 @@ post '/' do
 
   intent_request = AlexaRubykit::build_request(json_params)
 
+  case intent_request.type
+  when 'INTENT_REQUEST'
+    case intent_request.intent.fetch('name')
+    when 'ExpressOrder'
+      intent_response.add_speech("Du möchtest eine Bestellung aufgeben")
+    else
+      intent_response.add_speech("Mit dieser Anfrage kann ich leider nichts anfangen")
+    end
+  when 'LAUNCH_REQUEST'
+    intent_response.add_speech("Du hast eine Session gestartet")
+  else
+    intent_response.add_speech("Unbekannte Art von Intent")
+  end
+
   content_type :json
 
   response = AlexaRubykit::Response.new
